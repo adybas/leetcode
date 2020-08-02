64a1b9b38acdada10eb1c87324aa177a070320b4
Anna Dybas <a.m.dybas@gmail.com>
2020-08-01 23:46:37 -0400

    solved longest substring

--- 3_longest_substring_without_repeating_characters.js
+++ 3_longest_substring_without_repeating_characters.js
@@ -17,3 +17,28 @@
 // Explanation: The answer is "wke", with the length of 3. 
 //              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+var lengthOfLongestSubstring = function(s) {
+    let count = 0
+    
+    let i = 0
+    let j = 0
+    let n = s.length
+    let set = new Set
+    
+    while (i < n && j < n){
+        if(!set.has(s.charAt(j))){
+            set.add(s.charAt(j))
+            j++
+            count = Math.max(count, j - i)
+        } else {
+            set.delete(s.charAt(i))
+            i++
+        }
+    }
+    return count
+};
+
+lengthOfLongestSubstring("au") // => 2
+lengthOfLongestSubstring(" ") // => 1
+lengthOfLongestSubstring("bbbbbb") // => 1
+lengthOfLongestSubstring("abcabcbb") // => 3
