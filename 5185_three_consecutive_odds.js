8b441e627e4c1ce5cfa7e1e6f55c67091b60ab0a
Anna Dybas <a.m.dybas@gmail.com>
2020-08-15 23:10:06 -0400

    solved 5185 three consecutive odds

--- 5185_three_consecutive_odds.js
+++ 5185_three_consecutive_odds.js
@@ -17,3 +17,12 @@
 
 // 1 <= arr.length <= 1000
 // 1 <= arr[i] <= 1000
+
+var threeConsecutiveOdds = function(arr) {
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 1 && arr[i + 1] % 2 === 1 && arr[i + 2] % 2 === 1 ){
+          return true
+        } 
+      }
+      return false
+};
