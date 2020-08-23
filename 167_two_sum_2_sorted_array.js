bdefff2bf05f0e0c3d50c8da71f402dd65316d3c
Anna Dybas <a.m.dybas@gmail.com>
2020-08-23 18:46:48 -0400

    added two pointer solution to 167

--- 167_two_sum_2_sorted_array.js
+++ 167_two_sum_2_sorted_array.js
@@ -24,3 +24,19 @@ var twoSum = function(numbers, target) {
         }
     }
 };
+
+// Using Two pointers
+
+var twoSum = function(numbers, target) {
+    let pointer1 = 0
+    let pointer2 = numbers.length - 1
+
+    while(numbers[pointer1] + numbers[pointer2] !== target){
+        if (numbers[pointer1] + numbers[pointer2] < target){
+            pointer1 += 1
+        } else {
+            pointer2 -= 1
+        }
+    }
+    return [pointer1 + 1, pointer2 + 1]
+}
