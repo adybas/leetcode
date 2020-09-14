d6e20fce1fcecb7094319a416a68ed8cd2fe66ea
Anna Dybas <a.m.dybas@gmail.com>
2020-09-14 19:06:04 -0400

    used binary search on matrix (instead of brute force)

--- 74_search_2d_matrix.js
+++ 74_search_2d_matrix.js
@@ -25,3 +25,67 @@
 // target = 13
 // Output: false
 
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    // for (let i = 0; i< matrix.length; i++){
+    //     for (let j = 0; j < matrix[0].length; j++){
+    //         if (matrix[i][j] === target){
+    //             return true
+    //         }
+    //     }
+    // }
+    // return false
+    //}
+/*
+Time Complexity: O(n^2) or O(m*n)
+=> We have a nested for loop, so time complexity will depend on the size of n we iterate over (if row and column are same size) OR m * n (if row and column are different sizes)
+
+Space Complexity: O(1)
+=> We create a hash, which takes up n amount of space;
+*/
+    
+    if (matrix.length === 0) return false
+    
+    let row = matrix.length
+    let column = matrix[0].length
+    
+    let left = 0
+    let right = row*column - 1
+    
+    while (left <= right){
+        //calculate midpt and account for integer overflow
+        let midpoint = Math.floor(left + (right-left) / 2)
+        
+        //calculate row element and round down
+        let row_element = Math.floor(midpoint/column)
+        
+        //calculate col element to locate midpt element
+        let midpoint_element = matrix[row_element][midpoint%column]
+        
+        //do binary search
+        if(target === midpoint_element){
+            return true
+        } else if (target < midpoint_element){
+            right = midpoint - 1
+        } else if (target > midpoint_element){
+            left = midpoint + 1
+        }
+    }
+    return false
+};
+
+/*
+Time Complexity: O(log(n))
+=> Since we conducted a binary search, time complexity it log(n)
+
+Space Complexity: O(1)
+=> We create a hash, which takes up n amount of space;
+*/
+
+
+
+    
